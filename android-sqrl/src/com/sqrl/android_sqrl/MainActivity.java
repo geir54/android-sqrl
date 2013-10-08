addf28794971923298892d910d2271062e2983f1
geir <1>
2013-10-08 22:57:17 +0200

    app can now communicate with server

--- android-sqrl/src/com/sqrl/android_sqrl/MainActivity.java
+++ android-sqrl/src/com/sqrl/android_sqrl/MainActivity.java
@@ -1,11 +1,20 @@
 package com.sqrl.android_sqrl;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
 
 import com.sqrl.crypto.ed25519;
 
@@ -13,6 +22,7 @@ import eu.livotov.zxscan.ZXScanHelper;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Base64;
 import android.view.Menu;
@@ -22,6 +32,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 public class MainActivity extends Activity {
 	
 	private static byte[] Master_Key = "a secret psudo random number".getBytes();
@@ -29,6 +45,9 @@ public class MainActivity extends Activity {
 	private EditText editText1 = null;
 	private EditText editText2 = null;
 	private String URL = "";
+	
+	private String pubKey = "";
+	private String sign = "";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +66,7 @@ public class MainActivity extends Activity {
             	 new createSignature().execute(URL);
             }  });
         
-        ZXScanHelper.scan(this,12345);            
+        ZXScanHelper.scan(this,12345);               
     }
     
     // Jumps here when QR is scanned
@@ -60,7 +79,7 @@ public class MainActivity extends Activity {
             URL = removeHTTP(scannedCode);
             String domain = getDomain(URL);
             
-            textView1.setText("Authenticate to " + domain);
+            textView1.setText("Authenticate to " + domain);           
         }
     }
   
@@ -80,8 +99,13 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onPostExecute(String[]result) {
-        	editText1.setText(result[0]);        
-        	editText2.setText(result[1]);        
+        	pubKey = result[0];
+        	sign = result[1];
+        	editText1.setText(pubKey);        
+        	editText2.setText(sign);    
+        	
+        	// TODO: Get URL from scanned code
+        	web_post("http://sqrl.host56.com/sqrl_verify.php", URL, sign, pubKey);	
         }
       }
     
@@ -118,5 +142,40 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    // Send signature and pubkey to server
+    private void web_post(String URL, String message, String signature, String publicKey) {
+    	HttpClient httpclient = new DefaultHttpClient();
+	    HttpPost httppost = new HttpPost(URL);
+	  
+	    try {
+	        // Add data to post
+	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+	        nameValuePairs.add(new BasicNameValuePair("message", message));
+	        nameValuePairs.add(new BasicNameValuePair("signature", signature));
+	        nameValuePairs.add(new BasicNameValuePair("publicKey", publicKey));
+	    
+	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));	       	        
+	        
+	        HttpResponse response = httpclient.execute(httppost); // Execute HTTP Post Request
+	    	
+	        int status = response.getStatusLine().getStatusCode();
+	        
+	        Context context = getApplicationContext();
+	        if (status == HttpStatus.SC_OK) {
+	        	ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+	        	response.getEntity().writeTo(ostream);
+	    
+	        	String out = ostream.toString();
+	        
+	        	// See if the page returned "Verified"
+	        	if (out.contains("Verified")) {
+	        		Toast.makeText(context, "Verified", Toast.LENGTH_LONG).show(); // show the user	        		
+	        	}
+	        }      
+	    } catch (ClientProtocolException e) {	       
+	    } catch (IOException e) {	      
+	    }
     }
 }
